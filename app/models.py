b3f9ba2bd11b45c9562fab2091aa0cfb06ebd148
Areej Althagafi <althagafiareej2@gmail.com>
2026-09-09 03:20:10 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,20 @@
-from sqlalchemy import Column,Integer,String,Text,DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime
 from datetime import datetime
 from app.database import Base
+
 class PatientCase(Base):
-    __tablename__='patient_cases'
-    id=Column(Integer,primary_key=True,index=True)
-    symptoms_text=Column(Text,nullable=False)
-    detected_symptoms=Column(Text)
-    duration=Column(String)
-    severity=Column(String)
-    priority=Column(String)
-    doctor_summary=Column(Text)
-    created_at=Column(DateTime,default=datetime.utcnow)
+    __tablename__ = "patient_cases"
+
+    id = Column(Integer, primary_key=True, index=True)
+    symptoms_text = Column(Text, nullable=False)
+    age = Column(String)
+    gender = Column(String)
+    detected_symptoms = Column(Text)
+    duration = Column(String)
+    associated_symptoms = Column(Text)
+    danger_signs = Column(Text)
+    severity = Column(String)
+    priority = Column(String)
+    doctor_summary = Column(Text)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
